40c0da4a575f614b5a142e505fe09210eb4055f8
abduazam <abduazamdev@gmail.com>
2024-01-29 13:00:59 +0500

    prod 1.2: error handler added

---
+++ config/error-handler-bot.php
@@ -0,0 +1,29 @@
+<?php
+
+return [
+    /*
+   |--------------------------------------------------------------------------
+   | Telegram Bot API Token
+   |--------------------------------------------------------------------------
+   |
+   | This is your Telegram bot API Token to send error from right Telegram bot.
+   |
+   */
+
+    'bot-api-token' => "6622424841:AAFUtDh8-V2skCheCxsdiFxmIkyosutGx7g",
+
+    /*
+    |--------------------------------------------------------------------------
+    | Receiver channel/group
+    |--------------------------------------------------------------------------
+    |
+    | This is receiver message from your telegram bot.
+    |
+    */
+
+    'receiver' => [
+        'channel' => null,
+
+        'group' => -4136252606,
+    ]
+];
