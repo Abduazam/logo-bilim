a50861243665f9abf14a0db8c835d164bd28b749
abduazam <abduazamdev@gmail.com>
2024-03-14 15:32:47 +0500

    prod 1.5.5: seeder fixed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -28,46 +28,5 @@ class DatabaseSeeder extends Seeder
             Dashboard\Information\Statuses\Relatives\RelativeStatusSeeder::class,
             Dashboard\Information\Statuses\Appointments\AppointmentStatusSeeder::class,
         ]);
-
-        /**
-         * Create fake branches and services.
-         */
-        $branches = Branch::factory(3)->create();
-
-        $services = Service::factory(3)->create();
-
-        $services = $services->shuffle();
-
-        foreach ($branches as $branch) {
-            $assignedServices = $services->random(2);
-
-            foreach ($assignedServices as $service) {
-                BranchService::factory()->create([
-                    'branch_id' => $branch->id,
-                    'service_id' => $service->id,
-                    'price' => rand(70, 150) * 1000,
-                ]);
-            }
-        }
-
-        /**
-         * Create fake teachers.
-         */
-        $teachers = Teacher::factory(10)->create();
-
-        $combinations = collect();
-
-        foreach ($teachers as $teacher) {
-            foreach ($branches as $branch) {
-                foreach ($services as $service) {
-                    $combinations->push([
-                        'teacher_id' => $teacher->id,
-                        'branch_id' => $branch->id,
-                        'service_id' => $service->id,
-                        'salary' => rand(50, 100) * 1000,
-                    ]);
-                }
-            }
-        }
     }
 }
